993f1c462ea0f139d158e5418b7f29d939d6772a
Johan Vrolix <johan@vrolix.eu>
2016-12-29 09:36:57 +0100

    Added module settings

---
+++ src/settings.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+  'settings' => [
+    'services' => ['AuthService', '\Blueberry\Core\Service\AuthService'],
+    'middlewares' => ['AuthMiddleware', '\Blueberry\Core\Middleware\AuthMiddleware'],
+  ]
+]
